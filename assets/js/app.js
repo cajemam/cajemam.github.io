e2d9759e8d1fc3e6206cc87bcbe8543239fc3dc7
Cajemam Carente <106678041+cajemam@users.noreply.github.com>
2022-06-01 16:32:09 +0300

    Upload Website

---
+++ assets/js/app.js
@@ -0,0 +1,9 @@
+class App{
+    constructor(conf){
+        this.config = conf;
+    }
+
+    logg(){
+        return this.config;
+    }
+}
---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Cajemam Carente</title>
+        <meta charset="UTF-8"/>
+    </head>
+    <body>
+        <h1>Cajemam Carente</h1>
+        <script src="assets/js/app.js"></script>
+        <script>
+            var config = {
+                "style": "assets/css/main.scss",
+                "name" : "Cajemam Carente"
+            };
+            var app = new App(config);
+            app.logg();
+        </script>
+    </body>
