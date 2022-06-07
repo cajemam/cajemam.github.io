820ea28d501a1d64e8e772cc06c876401be2d9a1
Cajemam Carente <106678041+cajemam@users.noreply.github.com>
2022-06-07 08:22:27 +0300

    ddd

--- assets/js/auth.app.js
+++ assets/js/auth.app.js
@@ -1,18 +1,3 @@
-const firebaseConfig = {
-    apiKey: "AIzaSyDDiePq0IFWXflzwoSKpIEGCXCNCc1-DiE",
-    authDomain: "cajemam-carente.firebaseapp.com",
-    projectId: "cajemam-carente",
-    storageBucket: "cajemam-carente.appspot.com",
-    messagingSenderId: "652369637774",
-    appId: "1:652369637774:web:995371886087d07a87b4df",
-    measurementId: "G-2HHSBJPGGT"
-};
-
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-
-const auth = getAuth();
-
 document.querySelector('.googleAuthBtn').onclick  = function() {
     let provider = new GoogleAuthProvider();
     signInWithPopup(auth, provider)
